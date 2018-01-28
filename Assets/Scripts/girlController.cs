4b65ad75b91b8ae28b5c56fcfdf6f732b094e1e9
Yangzoom <Yangzoom@users.noreply.github.com>
2018-01-28 15:21:58 -0600

    Initial Commit

---
+++ Assets/Scripts/girlController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class girlController : MonoBehaviour {
+	private Rigidbody2D myRigidBody;
+	public float girlJumpForce = 500f;
+
+	// Use this for initialization
+	void Start () {
+		myRigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonUp("Jump")){
+		myRigidBody.AddForce(transform.up * girlJumpForce);
+	 }
+   }
+}
